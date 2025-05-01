abbb63fa1ef9d52e64e1b825043c3705151a2fcb
Kimchuljoong <wnmh123@gmail.com>
2025-05-01 23:29:24 +0900

    first commit

---
+++ src/test/kotlin/kr/co/kimga/course_catalog_service/CourseCatalogServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.kimga.course_catalog_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CourseCatalogServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
